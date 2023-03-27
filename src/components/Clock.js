165a6bed0a9618554bb71c3b382625d4e359751c
Aikol1991 <122163501+Aikol1991@users.noreply.github.com>
2023-03-27 14:19:03 +0600

    Add files via upload

---
+++ src/components/Clock.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+  return (
+    <div>
+      <h1>Current Time:</h1>
+      <p>{time}</p>
+    </div>
+  );
+}
+export default Clock;
